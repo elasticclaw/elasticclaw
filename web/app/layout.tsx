c18450db595243e084fc1fdb8d4e46ebf497f1a7
Marc Campbell <marc.e.campbell@gmail.com>
2026-05-10 11:47:01 +0000

    feat: replace favicon with lightning bolt icon

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -13,16 +13,14 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-lightning-light-32x32.png',
         media: '(prefers-color-scheme: light)',
+        type: 'image/png',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon-lightning-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
     apple: '/apple-icon.png',
